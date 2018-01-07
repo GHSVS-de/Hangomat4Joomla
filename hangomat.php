a09b53c0489091e3a500198b456f1901b36c4609
ReLater <ReLater@users.noreply.github.com>
2018-01-07 23:43:36 +0100

    Update hangomat.php

--- hangomat.php
+++ hangomat.php
@@ -1778,7 +1778,7 @@ class hangomatHelper
 	{
 		if ($this->db->getServerType() != 'mysql')
 		{
-			JFactory::getApplication()->enqueueMessage(JText::sprintf('HANGOMAT_DB_DRIVER_NOT_SUPPORTED', $this->db->serverType), 'error');
+			JFactory::getApplication()->enqueueMessage(JText::sprintf('HANGOMAT_DB_DRIVER_NOT_SUPPORTED', $this->db->getServerType()), 'error');
 			return false;
 		}
 		return true;
